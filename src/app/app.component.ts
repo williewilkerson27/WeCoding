71d801968409999090cf5911e76a0ac9911fe1e3
Wilky <williewilkerson27@gmail.com>
2024-11-26 18:39:19 -0500

    1st

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="../assets/wecoding_logo.svg" alt="logo" aria-hidden="true" />
+        <section class="content">
+          <app-home></app-home>
+        </section>
+      </header>
+    </main>
+  `,
+  styleUrl: './app.component.css',
+  imports: [HomeComponent]
 })
 export class AppComponent {
   title = 'WeCoding';
