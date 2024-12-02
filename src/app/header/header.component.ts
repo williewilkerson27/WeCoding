26b6ae10e0f2aeefca3e4ff407801611de9bf27d
Wilky <williewilkerson27@gmail.com>
2024-12-02 15:24:53 -0500

    Header optimized

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,14 +9,14 @@ import { Component } from '@angular/core';
       <div class="row">
         <div class="col-md-3 col-sm-3 col-3">
           <div class="logo-container">
-            <img src="assets/wecoding_logo.svg" alt="logo" aria-hidden="true">
+            <img src="assets/wecoding_logo_transparent.svg" class="logo" alt="logo" aria-hidden="true">
           </div>
         </div>
           <div class="col-md-9 col-sm-9 col-9 text-end">
-            <div class="menu-icon">
+            <div class="menu-icon" (click)="openMenu()">
               <i class="bi bi-list"></i>
             </div>
-            <div class="desktop-menu">
+            <div class="desktop-menu" [class.mobile-menu]="menuValue">
               <ul>
                 <li>Home</li>
                 <li>About</li>
@@ -34,5 +34,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuValue:boolean= true;
 
+  openMenu() {
+    this.menuValue = !this.menuValue;
+  }
 }
