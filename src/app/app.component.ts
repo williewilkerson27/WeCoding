6b66c49b2eaf65555def81e0f39d2ead5e0ed598
Wilky <williewilkerson27@gmail.com>
2024-12-09 19:30:47 -0500

    still needs fixing with the scss files and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    LandingPageComponent,
+    FooterComponent
+  ],
   template: `
     <app-navbar></app-navbar>
     <app-landing-page></app-landing-page>
     <app-footer></app-footer>
-  `
+  `,
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'wecoding';
 }
